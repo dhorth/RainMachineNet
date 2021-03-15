e6c169a89367b6baec40f49902e906fb94386f59
David Horth <dhorth@horth.com>
2021-03-15 14:59:55 -0400

    Serilog Instrumention

--- src/Lib/RainMachineNet/RainMachineLibBase.cs
+++ src/Lib/RainMachineNet/RainMachineLibBase.cs
@@ -1,4 +1,5 @@
 ﻿using RainMachineNet.Requests;
+using RainMachineNet.Support;
 using RestSharp;
 using RestSharp.Authenticators;
 using RestSharp.Serialization.Json;
@@ -17,19 +18,19 @@ namespace Horth.RainMachineNet
         protected string _accessToken;
 
         protected const string BaseUrl = "https://{0}:8080/api/4/";
-        protected const string DeviceCertId = "0D662453BAC4F5B5E874B4341B9E22B3270336CC";
 
         public RainMachineLibBase()
         {
         }
-        public void Initialize(string netName)
+        public void Initialize(string netName, string deviceCertId)
         {
+            Log.Debug($"Initialize({netName}) Rest client to communicate with local device");
             _client = new RestClient(string.Format(BaseUrl, netName));
             _client.RemoteCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
             {
                 var thumbprint = cert.GetCertHashString();
 
-                if (thumbprint == DeviceCertId)
+                if (thumbprint == deviceCertId)
                 {
                     Log.Information($"Allowing Device Cert ");
                     return true;
@@ -40,9 +41,11 @@ namespace Horth.RainMachineNet
                     return true;
                 };
             };
+            Log.Debug($"Initialize({netName}) complete");
         }
         public async Task<T> Execute<T>(string endPoint, RequestBase request) where T : new()
         {
+            Log.Debug($"Execute({endPoint}) helper");
             var restRequest = new RestRequest(endPoint, Method.POST);
             restRequest.AddParameter("application/json; charset=utf-8", request.ToJson(), ParameterType.RequestBody);
             restRequest.RequestFormat = DataFormat.Json;
@@ -52,17 +55,24 @@ namespace Horth.RainMachineNet
 
         public async Task<T> Execute<T>(RestRequest request) where T : new()
         {
+            Log.Debug($"Execute Request({request.Resource}) ");
             if (!string.IsNullOrEmpty(_accessToken))
+            {
                 request.AddQueryParameter("access_token", _accessToken); // used on every request but login
-
+            }
+            else
+            {
+                Log.Warning("Request being made without access token!");
+            }
             request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
+
             var response = await _client.ExecuteAsync<T>(request);
             if (response.ErrorException != null)
             {
-                const string message = "Error retrieving response.  Check inner details for more info.";
-                var twilioException = new Exception(message, response.ErrorException);
+                var twilioException = new RainMakerExecuteException(string.IsNullOrEmpty(_accessToken), response.ErrorException);
                 throw twilioException;
             }
+            Log.Debug($"Execute Request({request.Resource}) => {response.Data!=null} ");
             return response.Data;
         }
 
