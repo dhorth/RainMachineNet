56fb66f98dfcb121bf8c4e099eefcff83b14d3a4
David Horth <dhorth@horth.com>
2021-03-16 14:11:07 -0400

    console

--- src/Lib/RainMachineNet/Event/WateringNotificationSubscriber.cs
+++ src/Lib/RainMachineNet/Event/WateringNotificationSubscriber.cs
@@ -20,20 +20,17 @@ namespace RainMachineNet.Event
 
         public override void OnCompleted()
         {
-            Console.WriteLine("Done");
             Log.Information($"OnCompleted...");
         }
 
         public override void OnError(Exception ex)
         {
-            Console.WriteLine($"Error: {ex.Message}");
             Log.Error($"OnError...", ex);
         }
 
 
         public override void OnNext(T ev)
         {
-            Console.WriteLine($"Hey {SubscriberName} -> you received {ev.EventProviderName} {ev.Description} @ {ev.Date} ");
             Log.Information($"OnNext...");
         }
     }
